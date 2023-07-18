2b5b9aec2aced58f06ac36f1ec23c4821c8f87cc
JoJo-Rob <josueroberto099@gmail.com>
2023-07-18 16:12:31 -0300

    Adicionando arquivos projeto-javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.projetojavafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens br.com.projetojavafx to javafx.fxml;
+    exports br.com.projetojavafx;
+}
